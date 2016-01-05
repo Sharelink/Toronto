698acb7f25d012f1d14ad212f0fe29d3f5f8073b
AlexChow <cplover@live.com>
2016-01-05 19:48:27 +0800

    v2

---
+++ src/TorontoAPIServer/Controllers/SharelinkThemesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TorontoAPIServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class SharelinkThemesController : TorontoAPIController
+    {
+        // GET: api/values
+        [HttpGet("Recommend")]
+        public IEnumerable<object> GetRecommendThemes(string region)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
